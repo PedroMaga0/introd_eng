02a864e6ca36c24b3232f46584a22568a61802bd
PedroMaga0 <50275335+PedroMaga0@users.noreply.github.com>
2019-12-02 21:23:42 -0300

    Update teste2_intro.py

--- teste2_intro.py
+++ teste2_intro.py
@@ -9,13 +9,14 @@ entrada = input('insita o diretório do arquivo:')
 
 carregar_wav = wave.open(entrada, 'r')
 
-def indexar_wav():
+def indexar_wav(entrada):
     cont = 1
     for nome in os.listdir(entrada):
-       
+        nome = nome.split('.')
+
         novo_nome = cont + '.wav'
 
-        os.rename(entrada+nome, entrada+novo_nome)
+        nome.replace(nome[0], novo_nome)
         
         cont = cont + 1
 
